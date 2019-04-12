2cef7cb058b9315765910d175465ed2883b48f6f
dell <Paul>
2019-04-12 18:20:02 +0300

    ready header

--- wp-content/themes/twentynineteen/header.php
+++ wp-content/themes/twentynineteen/header.php
@@ -17,25 +17,26 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentynineteen' ); ?></a>
 	<div class="header">
 		<div class="container">
 			<div class="row">
-				<div class="col">
+				<div class="col logo-block">
 					<div class="logo">
-						<h2>logo</h2>
+						<h2><a href="<?php home_url() ?>">logo</a></h2>
 					</div>
 				</div>
-				<div class="col">
-					<div class="menu-block">
+				<div class="col menu-block">
+					<div class="menu">
 						<div class="menu-title">
-							<h2>menu</h2>
+							<h4>menu</h2>
 						</div>
 						<div class="burger-menu">
 							<span></span>
